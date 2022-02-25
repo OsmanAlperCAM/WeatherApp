55390c798789d24e5e52e909eaf1fe2e245f0493
osmanalpercam <63802259+OsmanAlperCAM@users.noreply.github.com>
2022-02-25 18:24:45 +0300

    Dashboard Page Template Created

---
+++ src/Pages/Dashboard/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './Layout.style';
+
+const Layout = props => {
+  return (
+    <View style={styles.container}>
+      <Text>Layout</Text>
+    </View>
+  );
+};
+export default Layout;
