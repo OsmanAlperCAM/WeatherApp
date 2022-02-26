f600043b41190398f2d5d83b4d9270cccf000ac4
osmanalpercam <63802259+OsmanAlperCAM@users.noreply.github.com>
2022-02-26 16:39:01 +0300

    colors.js file created

---
+++ src/styles/colors.js
@@ -0,0 +1,17 @@
+export default {
+  primary: {
+    color: '#42a5f5',
+    light: '#80d6ff',
+    dark: '#0077c2',
+  },
+  secondary: {
+    color: '#0d47a1',
+    light: '#5472d3',
+    dark: '#002171',
+  },
+  text: {
+    black: '#000000',
+    white: '#ffffff',
+  },
+  backgroundColor: '#37474f',
+};
